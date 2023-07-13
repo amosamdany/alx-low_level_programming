2cac2f4675673b0d3e9e0079312797a051ddb23c
amosamdany <amdany18@gmail.com>
2023-07-13 15:08:11 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,12 @@
 /**
 * main - Entry point
 *
-* Return: Always 0 (Success)
+*Return: Always 0 (Success)
 */
 
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,");
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+
 	return (0);
 }
