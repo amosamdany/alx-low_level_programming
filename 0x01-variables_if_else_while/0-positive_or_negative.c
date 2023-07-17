c85133d3c63da8b4a2edc88ac09b06e369b3555f
amosamdany <amdany18@gmail.com>
2023-07-17 10:57:21 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,19 +5,195 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
+int n;
+
+ srand(time(0));
+ n = rand() - RAND_MAX / 2;
+ if (n < 0){
+     printf("%d is negative", n);   
+ }
+ else if (n ==0 ) {
+	 printf("%d is Zero", n);   
+ }
+ else {
+	 printf("%d is positive", n);   
+ }
+	return (0);
+}
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
 	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n < 0){
-	 printf("%d is negative", n);   
-	}
-	else if (n ==0 ) {
-	 printf("%d is Zero", n);   
-	}
-   else  {
-	 printf("%d is positive", n);   
-   }
-	return (0);
+	 srand(time(0));
+	  n = rand() - RAND_MAX / 2;
+	   if (n < 0){
+		        printf("%d is negative", n);   
+			 }
+	    else if (n ==0 ) {
+		    	 printf("%d is Zero", n);   
+			  }
+	     else {
+		     	 printf("%d is positive", n);   
+			  }
+	     	return (0);
+}
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	 srand(time(0));
+	  n = rand() - RAND_MAX / 2;
+	   if (n < 0){
+		        printf("%d is negative", n);   
+			 }
+	    else if (n ==0 ) {
+		    	 printf("%d is Zero", n);   
+			  }
+	     else {
+		     	 printf("%d is positive", n);   
+			  }
+	     	return (0);
+}
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	 srand(time(0));
+	  n = rand() - RAND_MAX / 2;
+	   if (n < 0){
+		        printf("%d is negative", n);   
+			 }
+	    else if (n ==0 ) {
+		    	 printf("%d is Zero", n);   
+			  }
+	     else {
+		     	 printf("%d is positive", n);   
+			  }
+	     	return (0);
+}
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	 srand(time(0));
+	  n = rand() - RAND_MAX / 2;
+	   if (n < 0){
+		        printf("%d is negative", n);   
+			 }
+	    else if (n ==0 ) {
+		    	 printf("%d is Zero", n);   
+			  }
+	     else {
+		     	 printf("%d is positive", n);   
+			  }
+	     	return (0);
+}
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	 srand(time(0));
+	  n = rand() - RAND_MAX / 2;
+	   if (n < 0){
+		        printf("%d is negative", n);   
+			 }
+	    else if (n ==0 ) {
+		    	 printf("%d is Zero", n);   
+			  }
+	     else {
+		     	 printf("%d is positive", n);   
+			  }
+	     	return (0);
+}
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	 srand(time(0));
+	  n = rand() - RAND_MAX / 2;
+	   if (n < 0){
+		        printf("%d is negative", n);   
+			 }
+	    else if (n ==0 ) {
+		    	 printf("%d is Zero", n);   
+			  }
+	     else {
+		     	 printf("%d is positive", n);   
+			  }
+	     	return (0);
+}
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	 srand(time(0));
+	  n = rand() - RAND_MAX / 2;
+	   if (n < 0){
+		        printf("%d is negative", n);   
+			 }
+	    else if (n ==0 ) {
+		    	 printf("%d is Zero", n);   
+			  }
+	     else {
+		     	 printf("%d is positive", n);   
+			  }
+	     	return (0);
+}
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	 srand(time(0));
+	  n = rand() - RAND_MAX / 2;
+	   if (n < 0){
+		        printf("%d is negative", n);   
+			 }
+	    else if (n ==0 ) {
+		    	 printf("%d is Zero", n);   
+			  }
+	     else {
+		     	 printf("%d is positive", n);   
+			  }
+	     	return (0);
 }
 
